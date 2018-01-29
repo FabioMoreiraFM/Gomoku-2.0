525346551a54f6dbaaa3e5ba2102728d6a2a821b
Fabio Moreira <fabiomoreira.mf@gmail.com>
2018-01-29 20:36:30 -0200

    Add GUI
    New controller and view for GUI support

--- controller.py
+++ controller.py
@@ -6,40 +6,34 @@ class Controller:
 	_model = None
 	_view = None
 	_jogador_atual = Jogador.ADVERSARIO_2
+	_tipo_partida = None
+	_erro = False
 
 	def __init__(self):
 		self._model = model.Model()
-		self._view = view.View()
+		self._view = view.View(self)
+		self._view.menu_inicial()
 
-		self.iniciar_jogo(self._view.opcao_jogo())
+	def configuracoes_iniciais(self, partida, tamanho, dificuldade):
+		self._tipo_partida = partida
+		self._model.cria_tabuleiro(tamanho)
+		self._model.set_dificuldade(dificuldade)
 
-	def iniciar_jogo(self, opcao):
-		self._model.cria_tabuleiro(self._view.tamanho_tabuleiro())
-		self._view.exibir_tabuleiro(self._model.get_tabuleiro())
-		
-		if opcao:
-			self.partida_HxH()
-		else:
-			self.partida_HxIA()
+	def vez_humano(self, coord_nova_peca):
+		self._erro = False
+		self.mudar_adversario()
+		self.inserir_peca(coord_nova_peca)
+		return self._jogador_atual, self._erro
 
-		self._view.mensagem_fim_de_jogo(self._jogador_atual)
+	def vez_ia(self):
+		self.mudar_adversario()
+		coord = self._model.encontrar_melhor_jogada()
+		self.inserir_peca(coord)
+		return coord[0],coord[1]
 
-	def partida_HxH(self):
-		while self._model.fim_de_jogo():
-			self.mudar_adversario()
-			coord = self._view.escolha_posicao_inserir_peca()
-			self.inserir_peca(coord)			
-
-	def partida_HxIA(self):
-		while self._model.fim_de_jogo():
-			self.mudar_adversario()
-
-			if self._jogador_atual == Jogador.ADVERSARIO_1:
-				coord = self._view.escolha_posicao_inserir_peca()
-			else:
-				coord = self._model.encontrar_melhor_jogada()
-			
-			self.inserir_peca(coord)
+	def verifica_fim_jogo(self):
+		if self._model.fim_de_jogo():
+			self._view.mensagem_fim_de_jogo(self._jogador_atual) 
 
 	def mudar_adversario(self):
 		self._jogador_atual = Jogador(not self._jogador_atual.value)
@@ -50,8 +44,6 @@ class Controller:
 		except Exception:
 			self._view.msg_erro_inserir_peca()
 			self.mudar_adversario()
-		else:
-			self._view.exibir_tabuleiro(self._model.get_tabuleiro())
-			self._view.exibir_encadeamento(self._model.get_encadeamento())	
+			self._erro = True
 
 x = Controller()
--- view.py
+++ view.py
@@ -1,42 +1,147 @@
 # encoding: utf-8
+from tkinter import *
+from tkinter.messagebox import showinfo 
 from jogador import *
 
-class View:
-	def __init__(self):
-		return
+class View():
+    root = None
+    controller = None
+    frame_board = None
+    frame_info = None
+    frame_score = None
+    entry_nome = None
+    jogador_2 = None
+    entry_tamanho_tabuleiro = None
+    tamanho_tabuleiro = None
+    tipo_partida = False
+    jogador_vez = None
+    nome_jogador = ""
+    guarda_canvas = None
+    guarda_coord = None
 
-	def exibir_tabuleiro(self, tabuleiro):
-		tamanho = len(tabuleiro)
-		linha = ''
-		print '######TABULEIRO ATUAL########'
-		for y in range(tamanho-1):
-			for x in range(tamanho-1):
-				linha += str(tabuleiro[x][y].get_dono().value) + ' '	
-			print linha
-			linha = ''
+    def __init__(self, controller):
+        self.root = Tk()
+        self.root.title('Ultimate Gomoku Bamboozled')
 
-	def tamanho_tabuleiro(self):
-		try:
-			return int(raw_input("Qual o tamanho do tabuleiro? "))
-		except ValueError:
-			print "Insira um número válido"
-			return self.tamanho_tabuleiro()
+        self.controller = controller
 
-	def escolha_posicao_inserir_peca(self):
-		return [int(y) for y in raw_input("Escolha a coordenada para inserir a peça: ").split()]
+    def create_frame_board(self):
+        self.frame_board = Frame(self.root)
+        self.tamanho_tabuleiro = int(self.entry_tamanho_tabuleiro.get())
+        self.guarda_canvas = {}
+        self.guarda_coord = {}
+        rows = self.tamanho_tabuleiro
+        columns = self.tamanho_tabuleiro
+        for row in range(rows):
+            for column in range(columns):
+                canvas = Canvas(self.frame_board, width=15, height = 15, borderwidth=0, bg='blue')
+                canvas.bind("<Button-1>", self.board_click)
+                canvas.grid(row=row, column=column, padx=1, pady=1)
+                self.guarda_canvas[canvas] = [column, row]
+                self.guarda_coord[column,row] = canvas
 
-	def mensagem_fim_de_jogo(self, jogador_atual):
-		print "Parabéns " + str(jogador_atual) + ", você ganhou"
+        for column in range(columns):
+            self.frame_board.grid_columnconfigure(column, weight=1)
 
-	def opcao_jogo(self):
-		try:
-			return int(raw_input("Partida HxIA (0) - Partida HxH (1): "))
-		except ValueError:
-			print "Insira um número válido"
-			return self.opcao_jogo()
+        self.frame_board.grid(row=0, column=0, padx=10, pady=10)
 
-	def exibir_encadeamento(self, encadeamento):
-		print str(encadeamento[Jogador.ADVERSARIO_1]) + "    "+ str(encadeamento[Jogador.ADVERSARIO_2])
+    def board_click(self, event):
+        jogador_atual, erro = self.controller.vez_humano(self.guarda_canvas[event.widget])
+        if erro:
+            return
+        self.trocar_jogador_atual(jogador_atual)
+        
+        if jogador_atual == Jogador.ADVERSARIO_1:
+            event.widget.configure(bg='black')
+        else:
+            event.widget.configure(bg='white')
 
-	def msg_erro_inserir_peca(self):
-		print "Jogada inválida, tente novamente"
+        if not(self.tipo_partida):
+            self.guarda_coord[self.controller.vez_ia()].configure(bg='white')  
+
+        self.controller.verifica_fim_jogo()
+        self.trocar_jogador_atual(jogador_atual)
+
+    def create_frame_info(self):
+        self.frame_info = Frame(self.root)
+        self.frame_info.grid(row=0, column=1)
+
+        label_info = Label(self.frame_info, text="Custom Match")
+        label_info.grid(row=0,column=0, columnspan=4)
+
+        label_nome = Label(self.frame_info, text="Nome do Jogador: ")
+        label_nome.grid(row=1, column=0, sticky=W)
+
+        self.entry_nome = Entry(self.frame_info)
+        self.entry_nome.grid(row=1, column=1)
+
+        label_tamanho_tabuleiro = Label(self.frame_info, text="Tamanho do tabuleiro: ")
+        label_tamanho_tabuleiro.grid(row=2, column=0, sticky=W)
+
+        self.entry_tamanho_tabuleiro = Entry(self.frame_info)
+        self.entry_tamanho_tabuleiro.grid(row=2, column=1)
+
+        label_tipo_partida = Label(self.frame_info, text="Tipo de partida: ").grid(row=3, column=0, sticky=W)
+        radio_hh = Radiobutton(self.frame_info, text="PvP", variable="Tipo_partida", value=1, command=lambda: self.set_tipo_partida(True)).grid(row=3, column=1, sticky=W)
+        radio_hia = Radiobutton(self.frame_info, text="PvE", variable="Tipo_partida", value=2, command=lambda: self.set_tipo_partida(False)).grid(row=3, column=1, sticky=E)
+
+        label_dificuldade = Label(self.frame_info, text="Dificuldade: ").grid(row=4, column=0, sticky=W)
+        facil= Radiobutton(self.frame_info, text="Fácil", variable="dificuldade", value=1, command=lambda: self.set_dificuldade(3)).grid(row=4, column=1, sticky=W)
+        medio = Radiobutton(self.frame_info, text="Médio", variable="dificuldade", value=2, command=lambda: self.set_dificuldade(4)).grid(row=4, column=1, sticky=E)
+        dificil = Radiobutton(self.frame_info, text="Difícil", variable="dificuldade", value=3, command=lambda: self.set_dificuldade(5)).grid(row=4, column=2, sticky=E)
+
+        btn_inicio_jogo = Button(self.frame_info, text="Iniciar partida", command=self.iniciar_jogo)
+        btn_inicio_jogo.grid(row=5, column=0, columnspan=4)
+
+    def create_frame_score(self):
+        self.nome_jogador = self.entry_nome.get()
+        self.frame_info.destroy()
+
+        self.frame_score = Frame(self.root)
+        self.frame_score.grid(row=0, column=1)
+
+        label_title_score = Label(self.frame_score, text="Informações da partida").grid(row=0, column=0, columnspan=4)
+        tipo_partida = "PvP" if self.tipo_partida else "PvE"
+        label_title_partida = Label(self.frame_score, text="Tipo de partida: " + tipo_partida).grid(row=1, column=0, sticky=W)
+
+        label_nome_jogador = Label(self.frame_score, text="Nome Jogador 1: " + self.nome_jogador).grid(row=2, column=0, sticky=W)
+
+        
+        self.jogador_2 = "Cetoconazol" if self.tipo_partida else "Computador"
+
+        label_nome_jogador_2 = Label(self.frame_score, text="Nome Jogador 2: " + self.jogador_2).grid(row=3, column=0, sticky=W)
+
+        self.jogador_vez = StringVar()
+        self.jogador_vez.set("Esperando Jogador: " + self.nome_jogador)
+        label_vez_do_jogador = Label(self.frame_score, textvariable=self.jogador_vez).grid(row=4, column=0, sticky=W)
+
+    def menu_inicial(self):
+        self.create_frame_info()
+        self.root.mainloop()
+
+    def iniciar_jogo(self):
+        self.create_frame_board()
+        self.create_frame_score()
+        self.controller.configuracoes_iniciais(self.tipo_partida, self.tamanho_tabuleiro, self.dificuldade)
+
+    def set_tipo_partida(self, opcao):
+        self.tipo_partida = opcao
+
+    def trocar_jogador_atual(self, jogador):
+        self.jogador_vez.set("Esperando Jogador: {}".format(self.nome_jogador if jogador == Jogador.ADVERSARIO_2 else self.jogador_2))
+
+    def opcao_jogo(self):
+        return self.tipo_partida
+
+    def tamanho_tabuleiro(self):
+        return self.tamanho_tabuleiro
+
+    def mensagem_fim_de_jogo(self, jogador_vencedor):
+        showinfo("Game Over", "Parabéns {}, você ganhou!".format(self.nome_jogador if jogador_vencedor == Jogador.ADVERSARIO_1 else self.jogador_2))
+        self.root.destroy()
+
+    def msg_erro_inserir_peca(self):
+        showinfo("Jogada inválida", "Não é possível adicionar uma peça nessa posição!")
+    
+    def set_dificuldade(self, nivel_dificuldade):
+        self.dificuldade = nivel_dificuldade
