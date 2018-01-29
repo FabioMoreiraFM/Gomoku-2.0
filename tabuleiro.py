6bc0ba33451418d12ecfb393e010d62df6c1717f
Fabio Moreira <fabiomoreira.mf@gmail.com>
2018-01-29 20:38:30 -0200

    Nothing
    I don't know what git saw here.

--- tabuleiro.py
+++ tabuleiro.py
@@ -7,7 +7,7 @@ class Tabuleiro:
 	_nodos_disponiveis = None
 
 	def __init__(self, tamanho):
-		self._tabuleiro = [[nodo.Nodo([x,y], Jogador.ESPACO_LIVRE) for y in range(tamanho+1)] for x in range(tamanho+1)] 
+		self._tabuleiro = [[nodo.Nodo([x,y], Jogador.ESPACO_LIVRE) for y in range(tamanho+1)] for x in range(tamanho+1)]
 		self._criar_dict_nodos_disponiveis(tamanho)
 
 	def _criar_dict_nodos_disponiveis(self, tamanho):
@@ -15,7 +15,7 @@ class Tabuleiro:
 
 	def inserir_peca(self, coordenada, novo_dono):
 		nodo = self._tabuleiro[coordenada[0]][coordenada[1]]
-		
+
 		if (nodo.get_dono() == Jogador.ESPACO_LIVRE):
 			nodo.set_dono(novo_dono)
 			del self._nodos_disponiveis[(coordenada[0],coordenada[1])]
@@ -24,7 +24,7 @@ class Tabuleiro:
 
 	def remover_peca(self, nodo):
 		nodo.set_dono(Jogador.ESPACO_LIVRE)
-		self._nodos_disponiveis[(nodo.get_coordenada()[0], nodo.get_coordenada()[1])] = nodo	
+		self._nodos_disponiveis[(nodo.get_coordenada()[0], nodo.get_coordenada()[1])] = nodo
 
 	def get_nodos_disponiveis(self):
 		return self._nodos_disponiveis
