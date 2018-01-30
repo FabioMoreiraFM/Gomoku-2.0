d56f8103efd9014b28c437b0bf148483c6e7ec20
Fabio Moreira <fabiomoreira.mf@gmail.com>
2018-01-30 15:29:27 -0200

    Add draw
    And other things to adapt to the GUI.

--- controller.py
+++ controller.py
@@ -1,6 +1,6 @@
-import model
-import view
-from jogador import *
+from Model import model
+from View import view
+from Model.jogador import *
 
 class Controller:
 	_model = None
@@ -20,15 +20,28 @@ class Controller:
 		self._model.set_dificuldade(dificuldade)
 
 	def vez_humano(self, coord_nova_peca):
-		self._erro = False
 		self.mudar_adversario()
+		self._view.trocar_jogador_atual(self._jogador_atual)
+		self._erro = False
+		
 		self.inserir_peca(coord_nova_peca)
+		
+		if self.verifica_empate():
+			self._erro = True
+			self._view.mensagem_empate()
+
 		return self._jogador_atual, self._erro
 
 	def vez_ia(self):
 		self.mudar_adversario()
+		self._view.trocar_jogador_atual(self._jogador_atual)
+		
 		coord = self._model.encontrar_melhor_jogada()
 		self.inserir_peca(coord)
+
+		if self.verifica_empate():
+			self._view.mensagem_empate()
+
 		return coord[0],coord[1]
 
 	def verifica_fim_jogo(self):
@@ -44,6 +57,8 @@ class Controller:
 		except Exception:
 			self._view.msg_erro_inserir_peca()
 			self.mudar_adversario()
+			self._view.trocar_jogador_atual(self._jogador_atual)
 			self._erro = True
 
-x = Controller()
+	def verifica_empate(self):
+		return self._model.empate()	 
